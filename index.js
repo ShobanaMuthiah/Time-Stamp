e39b504e80310f8dae047b814d3f82ab0f149265
ShobanaMuthiah <shobanam222@gmail.com>
2024-05-29 18:37:35 -1200

    completed

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ app.use(express.json());
 // Create a new timestamp file
 app.get('/create', (req, res) => {
     let today = format(new Date(), 'dd-MM-yyyy hh-mm-ss');  //today stores the value of current data and time
-
+    const timestampDir = 'Timestamp';
+    if (!fs.existsSync(timestampDir)) {
+        fs.mkdirSync(timestampDir);
+    }
     //filepath denotes the path of the timestamps which are created and stored it as file named as the current data and time value
-    const filepath = `Timestamp/${today}.txt`; 
+    const filepath = `${timestampDir}/${today}.txt`; 
 
     //the file is created and named as the current date 
     fs.writeFileSync(filepath, `${today}`, 'utf8');
@@ -30,13 +33,18 @@ app.get('/create', (req, res) => {
 // Read all timestamp files
 app.get('/read', (req, res) => {
 
+    const timestampDir = 'Timestamp';
+    if (!fs.existsSync(timestampDir)) {
+        fs.mkdirSync(timestampDir);
+    }
+
     //read the Timestamp folder using the method  readdirSync 
-    const files = fs.readdirSync('Timestamp', 'utf8');
+    const files = fs.readdirSync(timestampDir, 'utf8');
 
     //All of the Timestamp files are stored into a data array by mapping the files
     const data= files.map(file => {
         //each file will be stored in filepath as array
-        const filepath = path.join('Timestamp', file);
+        const filepath = path.join(timestampDir, file);
 
 //reading the files which stored inside the filepath array and stored the array into timestamp
         const timestamp= fs.readFileSync(filepath, 'utf8');
